77e28b5007b5fd60b1f9b49a08b7103a49a75995
Aluno <Aluno@Digital.net>
2019-04-10 20:15:48 -0300

    alert em java

---
+++ Alert/alert/src/alert/CPU.java
@@ -0,0 +1,37 @@
+package alert;
+
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class CPU {
+
+    float tamanhoCPU;
+    float usadoCPU;
+    float restoCPU;
+
+    public void definirTamanhoCPU() {
+        this.tamanhoCPU = Float.parseFloat(JOptionPane.showInputDialog("pergunta cpu1 "));
+
+    }
+
+    public void definirQtndUsadoCPU() {
+        this.usadoCPU = Float.parseFloat(JOptionPane.showInputDialog("pergunta cpu2"));
+
+    }
+
+    public void definirRestoCPU() {
+        this.restoCPU = Float.parseFloat(JOptionPane.showInputDialog("pergunta cpu"));
+
+    }
+
+    public void alert() {
+        float metade = tamanhoCPU / 2;
+        if (usadoCPU <= metade) {
+            System.out.println("Ainda está tudo bem  com a sua CPU! ");
+        } else {
+            System.out.println("está utilizando d mais da sua CPU");
+        }
+
+    }
+
+}
