2483409cd45c78a2b2ab9c0c140e6f94413f2900
Natalia Tayoane <nataliatayoane@gmail.com>
2019-04-21 22:22:49 -0300

    correções

--- institucional/education/education/js/main.js
+++ institucional/education/education/js/main.js
@@ -419,3 +419,11 @@ $(document).ready(function() {
     });
 
 });
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $('.navbar').addClass('sticky');
+    } else {
+      $('.navbar').removeClass('sticky');
+    }
+  });
